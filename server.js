4386d8daf5e59cf1d9d7845af6a26b9e40edae21
MariahAdams <mariah.adams23@yahoo.com>
2018-07-08 17:07:48 -0700

    refactored code in server.js to include anonymous function calls

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http');
 
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello World!');
-    res.end();
-}).listen(8888);
+function onRequest(req, res) {
+    console.log('Request received.');
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+    res.write('Hello World');
+    res.end()
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started');
