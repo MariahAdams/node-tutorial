1e4ed7f238801859740a25bc69acf9aa3639727e
MariahAdams <mariah.adams23@yahoo.com>
2018-07-08 18:23:29 -0700

    code refactored to include responding request handlers with non-blocking operations

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,20 @@
-function start() {
+var exec = require('child_process').exec;
+
+function start(res) {
     console.log('Request handler \'start\' was called.');
-    return 'Hello Start';
+
+    exec('ls -lah', function(error, stdout, stderr) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write(stdout);
+        res.end();
+    });
 }
 
-function upload() {
+function upload(res) {
     console.log('Request handler \'upload\' was called.');
-    return 'Hello Upload';
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello Upload');
+    res.end();
 }
 
 exports.start = start;
