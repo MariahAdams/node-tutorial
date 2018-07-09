da5cb84d6b748cdccfaebdea2c5f077f06cb3396
MariahAdams <mariah.adams23@yahoo.com>
2018-07-08 17:17:38 -0700

    added logic needed to find out URL path that was requested

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var http = require('http');
+var url = require('url');
 
 function start() {
     function onRequest(req, res) {
-        console.log('Request received.');
+        var pathname = url.parse(req.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
         res.writeHead(200, { 'Content-Type': 'text/plain'});
         res.write('Hello World');
         res.end();
     }
 
     http.createServer(onRequest).listen(8888);
-
     console.log('Server has started');
 }
 
